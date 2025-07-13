10028e2e0573a20535f7a4f88f0afda4224f8018
osama alqutob <osamaqutob11@gmail.com>
2025-07-13 15:47:06 +0300

    Implement CQRS abstraction Add mediatR Add Carter

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    //To register all command and query handlers in current project.
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
-var app = builder.Build(); 
+var app = builder.Build();
 
 // configure the HTTP request pipeline.
-
+app.MapCarter();
 app.Run();
